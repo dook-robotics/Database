c9167716af7a210855c0019daf5ed1891743ca8f
Mikian Musser <Bobar312@gmail.com>
2019-09-17 18:42:38 -0700

    Added CSVs, renamed images

---
+++ scripts/uniqueid.py
@@ -0,0 +1,27 @@
+import glob
+import os
+import sys
+
+print("Active:", str(sys.argv[1]))
+images = glob.glob(str(sys.argv[1]))
+print("Total Images:", len(images))
+
+count = 0
+for file in images:
+    src = file.replace('\\', '/')
+    name = src.split('/')
+    folder = name[len(name) - 2]
+    name = name[len(name) - 1]
+    if not "sg" in name and not "t" in name and not "tg" in name:
+        dst = src
+        if(folder == "singleGrid"):
+            dst = dst.replace(name, "sg" + name)
+        if(folder == "tripple"):
+            dst = dst.replace(name, "t" + name)
+        if(folder == "trippleGrid"):
+            dst = dst.replace(name, "tg" + name)
+        os.rename(src, dst)
+        count = count + 1
+        print(dst)
+
+print("Images renamed:", count)
